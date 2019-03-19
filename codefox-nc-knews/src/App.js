60f08d951ae14d57b37dac49ca070fd06958e8fc
theCodeFox <akkitta@live.co.uk>
2019-03-19 09:06:35 +0000

    nav bar basics complete and css used to make elements more readabvle

--- codefox-nc-knews/src/App.js
+++ codefox-nc-knews/src/App.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import { Router, Link } from '@reach/router'
 import './App.css';
+import Home from './components/Home';
+import Topics from './components/Topics';
+import Comments from './components/Comments';
+import Users from './components/Users';
+import { fetchData } from './utils/app-utils';
 
 class App extends Component {
+  state = {
+    user: "",
+    users: [],
+    topics: [],
+    articles: []
+  }
+  // https://thecodefox-nc-knews.herokuapp.com/
+  componentDidMount = () => {
+    const users = fetchData('users');
+    const topics = fetchData('topics');
+    const articles = fetchData('articles');
+    return Promise.all([users, topics, articles])
+    .then(([users, topics, articles]) => {
+      return this.setState({
+        users: users,
+        topics: topics,
+        articles: articles
+      })
+    })
+  }
+
   render() {
+    const { user, topics, articles } = this.state;
     return (
       <div className="App">
-        
+        <nav className="nav">
+          <Link to="/" className="nav-link">Home</Link>
+          <Link to="/topics" className="nav-link">Topics</Link>
+          <Link to="/comments" className="nav-link">Comments</Link>
+          <Link to="/users" className="nav-link">Users</Link>
+          {/* <Link to="/login" className="nav-link"> */}
+          {user || `Anonymous User`}
+          {/* </Link> */}
+        </nav>
+        <Router className="main">
+          <Home path="/" />
+          <Topics path="/topics" topics={topics} articles={articles} />
+          <Comments path="/comments" />
+          <Users path="/users" />
+        </Router>
+        <nav className="nav">
+          im a footer
+        </nav>
       </div>
     );
   }
