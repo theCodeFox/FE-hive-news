dbff727070b371668ec3de37460565e8171fd048
theCodeFox <akkitta@live.co.uk>
2019-03-19 11:43:07 +0000

    user avatar bug fixed

--- codefox-nc-knews/src/components/User.jsx
+++ codefox-nc-knews/src/components/User.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Img from 'react-image';
+import defaultAvatar from '../images/nc-knews-default-avatar.png'
 
 const User = ({ user }) => {
-    const defaultAvatar = 'https://cdn-images-1.medium.com/max/1200/1*LdnSztHVYhhd8K8EqlgCJQ.png'
   return <li className="user">
     <span><h3>{user.username}</h3>
     <p>{user.name}</p></span>
--- codefox-nc-knews/src/components/Users.jsx
+++ codefox-nc-knews/src/components/Users.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import User from './User';
 
 const Users = ({ users }) => {
-// avatar_url: "https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg"
-// name: "Tom Tickle"
-// username: "tickle122"
     return <main className="users-comments-grid">
         <p className="users-comments-search">search user</p>
         <p className="users-comments-info">user info box</p>
