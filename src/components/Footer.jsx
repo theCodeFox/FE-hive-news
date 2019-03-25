7d0c69459ba0484067903e4b58f90ad91c130f2f
theCodeFox <akkitta@live.co.uk>
2019-03-25 13:07:49 +0000

    footer bug fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return <nav className="nav nav-bottom">
-    <a href="https://www.linkedin.com/in/kay-vose-codefox" className="footer-link">
+    <a href="https://www.linkedin.com/in/kay-vose-codefox" target="_blank" rel="noopener noreferrer" className="footer-link">
       -Kay Vose@LinkedIn-
     </a>
-    <a href="https://github.com/theCodeFoxtheCodeFox-NC-Knews" className="footer-link">
+    <a href="https://github.com/theCodeFox/theCodeFox-NC-Knews" target="_blank" rel="noopener noreferrer" className="footer-link">
       -theCodeFox@GitHub-
     </a>
   </nav>
